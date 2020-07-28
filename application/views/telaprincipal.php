d7d061e67b2b97aeaf5e2cde1fb45b5776e9a35a
emersonleo <emerson.leonardo@ufrpe.br>
2020-07-28 16:52:18 -0300

    resolvendo problema de crash na tela inicial

--- application/views/telaprincipal.php
+++ application/views/telaprincipal.php
@@ -11,6 +11,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>Bem-vindo</title>
 	<link rel="icon" href="http://localhost/poke/assets/img/icon.png" >
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+
 
 </head>
 <body style="background: #FDF2EC">
@@ -31,7 +33,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			        </a>
 			        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 			          <a class="dropdown-item" href="#">Alterar Conta</a>
-			          <a class="dropdown-item" href="#">Excluir Conta</a>
+			          <a class="dropdown-item" id="btnDelete" href="#">Excluir Conta</a>
 			        </div>
 		    	</li>
 		    	<li class="nav-item">
@@ -53,6 +55,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
 	</footer>
 
 </body>
@@ -61,22 +65,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <script type="text/javascript">
 	function construirCard(pokemon,numero){
 		console.log(pokemon)
-		var numeroFormatado = ("0000" + (numero + 1)).slice(-4);
+		var numeroFormatado = "#" + ("0000" + (numero + 1)).slice(-4);
 		var nome = pokemon.name[0].toUpperCase() + pokemon.name.substring(1,pokemon.name.length)
-		var strCard = "<div class='card' style='width:10%; height:10%; margin:5px; text-align:center; padding-top:0px;'><br> " + 
-		"<img class='pb-0' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (numero + 1).toString() + ".png'>" +
-		"<br><p class='row'> " + nome + "</p><br>" 
-		+ '<p class="row">' + numeroFormatado + '</p>'
-		"<br></div>"
+		var imagemLink = '"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + (numero + 1).toString() + '.png"'
+		var strCard = "<div lass='card' style='width:10%; height:10%; margin:5px; text-align:center; padding-top:0px;'>" + 
+		"<img class='pb-0' src= " + imagemLink +  ">" + nome + "<br>" + numeroFormatado + "</div>"
 		return strCard;
 	}
 	$(function(){
-			$.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000", function(data){
+			$.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151", function(data){
 				var pokemons = data.results;
 				var strBody = "";
-				for (var i = 0; i < 150; i++) {
+				for (var i = 0; i < 151; i++) {
+					console.log(i)
 					var pokemon = pokemons[i];
-					console.log(pokemons[i])
 					strBody += construirCard(pokemon,i);
 				}
 				$('#divcollection')[0].innerHTML = strBody
@@ -85,4 +87,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	$('#btnSair').click(function(){
 		window.location.href = <?php echo '"'.base_url('sair').'"'; ?>
 	})
+	
+
 </script>
